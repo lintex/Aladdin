b83b30e2f5ca3b68b028674c8dc4b5d5699f0d87
lintex <ceo@ixxj.com>
2016-01-28 14:34:45 +0800

    短信验证

--- app/src/main/java/com/ixxj/aladdin/Fragment3.java
+++ app/src/main/java/com/ixxj/aladdin/Fragment3.java
@@ -6,14 +6,50 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.HashMap;
+
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
+import cn.smssdk.gui.RegisterPage;
 
 /**
  * Created by lintex on 2016/1/13.
  */
 public class Fragment3 extends Fragment {
+    private View view=null;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.view3,container,false);
+        view = inflater.inflate(R.layout.view3, container, false);
+        SMSSDK.initSDK(getActivity(), "f3824f847d66", "1d05e3fa2b5a021b66350644965e462c");
+
+        Button btn = (Button) view.findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //打开注册页面
+                RegisterPage registerPage = new RegisterPage();
+                registerPage.setRegisterCallback(new EventHandler() {
+                    public void afterEvent(int event, int result, Object data) {
+// 解析注册结果
+                        if (result == SMSSDK.RESULT_COMPLETE) {
+                            @SuppressWarnings("unchecked")
+                            HashMap<String, Object> phoneMap = (HashMap<String, Object>) data;
+                            String country = (String) phoneMap.get("country");
+                            String phone = (String) phoneMap.get("phone");
+
+// 提交用户信息
+                            //registerUser(country, phone);
+                        }
+                    }
+                });
+                registerPage.show(getActivity());
+            }
+        });
+
+        return view;
     }
 }
